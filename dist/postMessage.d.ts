c0e62b1ad6aa7af05d7ddcd471eb86d35004a53f
TecAbel <abelardo96.work@gmail.com>
2024-08-25 22:31:51 -0600

    feat(update): build

--- dist/postMessage.d.ts
+++ dist/postMessage.d.ts
@@ -1,2 +1,2 @@
-export declare function listenMessage<T>(origin: URL, callback: (response: T | null) => {}): void;
+export declare function listenMessage<T>(origin: URL, callback: (response: T | null) => void): void;
 export declare function postMessage(targetOrigin: string, messageContent: string): void;
